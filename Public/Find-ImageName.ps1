fab1a25878f31513493016778f47ffefcc89f1d5
Rasmus Jelsgaard <rasmus.jelsgaard@3shape.com>
2019-10-15 10:41:07 +0200

    Lowercase the image name to ensure it is useful as an image name.

--- Public/Find-ImageName.ps1
+++ Public/Find-ImageName.ps1
@@ -11,5 +11,5 @@ function Find-ImageName {
         throw "No such git config: $gitConfigExists"
     }
     $result = Invoke-Command "git config --file `"$gitConfigPath`" --get remote.origin.url"
-    Find-RepositoryName -RepositoryPath $result.Output
+    (Find-RepositoryName -RepositoryPath $result.Output).ToLower()
 }
