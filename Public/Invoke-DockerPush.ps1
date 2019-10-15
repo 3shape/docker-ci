684dfd659a478217bb598379e648a0a563b3c246
Rasmus Jelsgaard <rasmus.jelsgaard@3shape.com>
2019-10-15 16:51:26 +0200

    Sunshine scenarios

--- Public/Invoke-DockerPush.ps1
+++ Public/Invoke-DockerPush.ps1
@@ -1,8 +1,22 @@
+. "$PSScriptRoot\..\Private\Utilities.ps1"
+
 function Invoke-DockerPush {
     [CmdletBinding()]
     param (
-
+        [ValidateNotNull()]
+        [string]
+        $Registry = '',
+        [ValidateNotNull()]
+        [string]
+        $Repository = '',
+        [string]
+        $ImageName,
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Tag = 'latest'
     )
-
-
+    $postfixedRegistry = Add-PostFix $Registry
+    $postfixedRepository = Add-Postfix $Repository
+    $command = "docker push ${postfixedRegistry}${postfixedRepository}${ImageName}:${Tag}"
+    Invoke-Command $command
 }
--- Tests/Invoke-DockerPush.Tests.ps1
+++ Tests/Invoke-DockerPush.Tests.ps1
@@ -10,28 +10,61 @@ Describe 'docker push' {
 
     BeforeEach {
         Initialize-MockReg
-        $code = {
-            Write-Debug $Command
-            StoreMockValue -Key "Invoke-Command" -Value $Command
-        }
-        Mock -CommandName "Invoke-Command" $code -Verifiable -ModuleName $script:moduleName
+
     }
 
     Context 'Push an image' {
 
-        It 'produces the correct command to invoke' {
-            Invoke-DockerLogin -Username "Mocked" -Password (ConvertTo-SecureString 'MockedPassword' –asplaintext –force)
-            $result = GetMockValue -Key 'Invoke-Command'
-            $result | Should -BeLikeExactly 'Write-Output "MockedPassword" | docker login --username "Mocked" --password-stdin'
+        It 'produces the correct command to invoke with only image name provided' {
+            $code = {
+                StoreMockValue -Key "Invoke-Command" -Value $Command
+            }
+            Mock -CommandName "Invoke-Command" $code -Verifiable -ModuleName $script:moduleName
+
+            Invoke-DockerPush -Image 'cool-image'
+            $mockResult = GetMockValue -Key 'Invoke-Command'
+
+            Assert-MockCalled -CommandName 'Invoke-Command' -ModuleName $script:moduleName
+            $mockResult | Should -Be "docker push cool-image:latest"
         }
-    }
 
-    Context 'Login to specific docker registry' {
+        It 'produces the correct command to invoke with image name and repository provided' {
+            $code = {
+                StoreMockValue -Key "Invoke-Command" -Value $Command
+            }
+            Mock -CommandName "Invoke-Command" $code -Verifiable -ModuleName $script:moduleName
 
-        It 'produced the correct command to invoke' {
-            Invoke-DockerLogin -Registry 'my.docker.registry' -Username "Mocked" -Password (ConvertTo-SecureString 'MockedPassword' –asplaintext –force)
-            $result = GetMockValue -Key 'Invoke-Command'
-            $result | Should -BeLikeExactly 'Write-Output "MockedPassword" | docker login --username "Mocked" --password-stdin my.docker.registry'
+            Invoke-DockerPush -Image 'cool-image' -Repository 'thebestdockerimages'
+            $mockResult = GetMockValue -Key 'Invoke-Command'
+
+            Assert-MockCalled -CommandName 'Invoke-Command' -ModuleName $script:moduleName
+            $mockResult | Should -Be "docker push thebestdockerimages/cool-image:latest"
+        }
+
+        It 'produces the correct command to invoke with image name, registry and repository provided' {
+            $code = {
+                StoreMockValue -Key "Invoke-Command" -Value $Command
+            }
+            Mock -CommandName "Invoke-Command" $code -Verifiable -ModuleName $script:moduleName
+
+            Invoke-DockerPush -Image 'cool-image' -Repository 'thebestdockerimages' -Registry 'hub.docker.com:1337'
+            $mockResult = GetMockValue -Key 'Invoke-Command'
+
+            Assert-MockCalled -CommandName 'Invoke-Command' -ModuleName $script:moduleName
+            $mockResult | Should -Be "docker push hub.docker.com:1337/thebestdockerimages/cool-image:latest"
+        }
+
+        It 'produces the correct command to invoke with image name, registry and repository provided' {
+            $code = {
+                StoreMockValue -Key "Invoke-Command" -Value $Command
+            }
+            Mock -CommandName "Invoke-Command" $code -Verifiable -ModuleName $script:moduleName
+
+            Invoke-DockerPush -Image 'cool-image' -Repository 'thebestdockerimages' -Registry 'hub.docker.com:1337' -Tag 'v1.0.3'
+            $mockResult = GetMockValue -Key 'Invoke-Command'
+
+            Assert-MockCalled -CommandName 'Invoke-Command' -ModuleName $script:moduleName
+            $mockResult | Should -Be "docker push hub.docker.com:1337/thebestdockerimages/cool-image:v1.0.3"
         }
     }
 }
