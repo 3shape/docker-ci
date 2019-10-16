b4ff3fabd123fb4341b8758cbb3194975e2c4a46
Rasmus Jelsgaard <jelsgaard@gmail.com>
2019-10-16 12:18:58 +0200

    Docker Push (#19)
    * Initial version of the feature.
    
    * Sunshine scenarios
    
    * Make image mandatory.
    
    * Code review changes.

---
+++ Public/Invoke-DockerPush.ps1
@@ -0,0 +1,26 @@
+. "$PSScriptRoot\..\Private\Utilities.ps1"
+
+function Invoke-DockerPush {
+    [CmdletBinding()]
+    param (
+        [ValidateNotNull()]
+        [string]
+        $Registry = '',
+
+        [ValidateNotNull()]
+        [string]
+        $Repository = '',
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ImageName,
+
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Tag = 'latest'
+    )
+    $postfixedRegistry = Add-PostFix $Registry
+    $postfixedRepository = Add-Postfix $Repository
+    $command = "docker push ${postfixedRegistry}${postfixedRepository}${ImageName}:${Tag}"
+    Invoke-Command $command
+}
---
+++ Tests/Invoke-DockerPush.Tests.ps1
@@ -0,0 +1,53 @@
+Import-Module -Force $PSScriptRoot/../Docker.Build.psm1
+Import-Module -Global -Force $PSScriptRoot/MockReg.psm1
+. "$PSScriptRoot\..\Private\CommandResult.ps1"
+
+Describe 'docker push' {
+
+    BeforeAll {
+        $script:moduleName = (Get-Item $PSScriptRoot\..\*.psd1)[0].BaseName
+    }
+
+    BeforeEach {
+        Initialize-MockReg
+        $code = {
+            StoreMockValue -Key "Invoke-Command" -Value $Command
+        }
+        Mock -CommandName "Invoke-Command" $code -Verifiable -ModuleName $script:moduleName
+    }
+
+    AfterEach {
+        Assert-MockCalled -CommandName 'Invoke-Command' -ModuleName $script:moduleName
+    }
+
+    Context 'Push an image' {
+
+        It 'produces the correct command to invoke with only image name provided' {
+            Invoke-DockerPush -Image 'cool-image'
+
+            $mockResult = GetMockValue -Key 'Invoke-Command'
+            $mockResult | Should -Be "docker push cool-image:latest"
+        }
+
+        It 'produces the correct command to invoke with image name and repository provided' {
+            Invoke-DockerPush -Image 'cool-image' -Repository 'thebestdockerimages'
+
+            $mockResult = GetMockValue -Key 'Invoke-Command'
+            $mockResult | Should -Be "docker push thebestdockerimages/cool-image:latest"
+        }
+
+        It 'produces the correct command to invoke with image name, registry and repository provided' {
+            Invoke-DockerPush -Image 'cool-image' -Repository 'thebestdockerimages' -Registry 'hub.docker.com:1337'
+
+            $mockResult = GetMockValue -Key 'Invoke-Command'
+            $mockResult | Should -Be "docker push hub.docker.com:1337/thebestdockerimages/cool-image:latest"
+        }
+
+        It 'produces the correct command to invoke with image name, registry, repository and tag provided' {
+            Invoke-DockerPush -Image 'cool-image' -Repository 'thebestdockerimages' -Registry 'hub.docker.com:1337' -Tag 'v1.0.3'
+
+            $mockResult = GetMockValue -Key 'Invoke-Command'
+            $mockResult | Should -Be "docker push hub.docker.com:1337/thebestdockerimages/cool-image:v1.0.3"
+        }
+    }
+}
