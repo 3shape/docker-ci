2fa359781706e48e818c2522cd951fa20d9fc04a
Rasmus Wihlborg Jelsgaard <jelsgaard@gmail.com>
2019-10-18 15:59:06 +0200

    Cleanup, remove unused code

--- Private/Test-DockerInstalled.ps1
+++
@@ -1,9 +0,0 @@
-function Test-DockerInstalled {
-    [CmdletBinding()]
-    param (
-        [ValidateNotNullOrEmpty()]
-        [String] $DockerCommand = 'docker'
-    )
-
-    Invoke-Command $DockerCommand
-}
--- Private/Test-GitInstalled.ps1
+++
@@ -1,8 +0,0 @@
-function Test-GitInstalled {
-    [CmdletBinding()]
-    param (
-        [ValidateNotNullOrEmpty()]
-        [String] $GitCommand = 'git --version'
-    )
-    Invoke-Command $GitCommand
-}
