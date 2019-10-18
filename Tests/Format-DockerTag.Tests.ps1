03e180c685272a6c1eef4bc3b2d4d6dde862f97b
Tuk <leekhangyeen@gmail.com>
2019-10-18 13:27:22 +0200

    Update Tests/Format-DockerTag.Tests.ps1
    Co-Authored-By: Rasmus Jelsgaard <jelsgaard@gmail.com>

--- Tests/Format-DockerTag.Tests.ps1
+++ Tests/Format-DockerTag.Tests.ps1
@@ -31,7 +31,7 @@ Describe 'Parse version, distro and arch from Dockerfile path' {
 
     Context 'Given a unsupported directory structure but an existing Dockerfile' {
 
-        It 'throws an exception, when the Dockerfile not in ContextPath' {
+        It 'throws an exception, when the Dockerfile is not in ContextPath' {
             $offContextDockerfile = Join-Path $exampleReposPath "/Off.Context.Dockerfile"
             $code = { Format-DockerTag -ContextRoot (Join-Path $exampleReposPath '3.0') -Dockerfile $offContextDockerfile }
             $code | Should -Throw -ExceptionType ([System.IO.FileNotFoundException]) -PassThru
