9ff68df05b708aa785c9b160e723253b3e2c0f36
Khang Yeen Lee <KhangYeen.Lee@3shape.com>
2019-10-18 10:44:45 +0200

    Fixing test to pass on Linux.

--- Tests/Test-IsSubdirectoryOf.Tests.ps1
+++ Tests/Test-IsSubdirectoryOf.Tests.ps1
@@ -24,7 +24,7 @@ Describe 'test if child path is a subdirectory of path' {
         }
 
         It 'fails when child path is NOT part of path' {
-            $childPath = Split-Path -Qualifier (Get-Location)
+            $childPath = (Get-Item -Path $PSScriptRoot).PSDrive.Root
             $result = Test-IsSubdirectoryOf -Path $parentPath -ChildPath $childPath
             $result | Should -Be $false
         }
