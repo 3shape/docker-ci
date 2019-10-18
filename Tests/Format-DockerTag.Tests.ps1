1cc0e27b598ff4db01ffcfadb4dd21179853be05
Tuk <leekhangyeen@gmail.com>
2019-10-18 13:45:13 +0200

    Format-DockerTag requires ContextRoot (#28)
    * Initial commit for the improvement.
    * ContextRoot works with tests.
    * Using Hello world docker image.
    * Update Tests/Format-DockerTag.Tests.ps1
    * 3 level folder structure is required to use Format-DockerTag correctly.
    * Updated tests to reflect code changes.

--- Tests/Format-DockerTag.Tests.ps1
+++ Tests/Format-DockerTag.Tests.ps1
@@ -8,28 +8,39 @@ Describe 'Parse version, distro and arch from Dockerfile path' {
         $script:moduleName = (Get-Item $PSScriptRoot\..\*.psd1)[0].BaseName
     }
 
+    $testData = Join-Path (Split-Path -Parent $PSScriptRoot) "Test-Data"
+    $exampleReposPath = Join-Path $testData "ExampleRepos"
+
     Context 'Given a well-formed directory structure' {
-        $testData = Join-Path (Split-Path -Parent $PSScriptRoot) "Test-Data"
-        $exampleReposPath = Join-Path $testData "ExampleRepos"
 
         It 'Can parse tool version, distro and arch' {
             $dockerFile = Join-Path $exampleReposPath '/3.0/servercore/amd64/Dockerfile'
-            $result = Format-DockerTag -Dockerfile $dockerFile
+            $result = Format-DockerTag -ContextRoot $exampleReposPath -Dockerfile $dockerFile
             $result.Tag | Should -Be '3.0-servercore-amd64'
             $result.Distro | Should -Be 'servercore'
             $result.Version | Should -Be '3.0'
             $result.Arch | Should -be 'amd64'
         }
+
+        It 'throws an exception, when Dockerfile is not found' {
+            $noSuchDockerFile = Join-Path $exampleReposPath "NotADockerFile"
+            $code = { Format-DockerTag -ContextRoot $exampleReposPath -Dockerfile $noSuchDockerFile }
+            $code | Should -Throw -ExceptionType ([System.IO.FileNotFoundException]) -PassThru
+        }
     }
 
-    Context 'Given a well-formed directory structure but non-existing Dockerfile' {
-        $testData = Join-Path (Split-Path -Parent $PSScriptRoot) "Test-Data"
-        $exampleReposPath = Join-Path $testData "ExampleRepos"
+    Context 'Given a unsupported directory structure but an existing Dockerfile' {
 
-        It 'throws an exception' {
-            $noSuchDockerFile = Join-Path $exampleReposPath "NotADockerFile"
-            $code = { Format-DockerTag -Dockerfile $noSuchDockerFile }
-            $code | Should -Throw -ExceptionType ([System.IO.FileNotFoundException]) -PassThru
+        It 'throws an exception, when the Dockerfile is not in ContextPath' {
+            $offContextDockerfile = Join-Path $exampleReposPath "/Off.Context.Dockerfile"
+            $code = { Format-DockerTag -ContextRoot (Join-Path $exampleReposPath '3.0') -Dockerfile $offContextDockerfile }
+            $code | Should -Throw -ExceptionType ([System.ArgumentException]) -PassThru
+        }
+
+        It 'throws an exception, when folder structure is not exactly 3 levels' {
+            $unsupportedPathDockerFile = Join-Path $exampleReposPath "/3.0/servercore/Unsupported.Path.Dockerfile"
+            $code = { Format-DockerTag -ContextRoot $exampleReposPath -Dockerfile $unsupportedPathDockerFile }
+            $code | Should -Throw -ExceptionType ([System.Management.Automation.RuntimeException]) -PassThru
         }
     }
 
