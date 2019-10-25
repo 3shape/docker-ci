a8eba879c7247562954834c5936450d21b2c7b11
Rasmus Wihlborg Jelsgaard <jelsgaard@gmail.com>
2019-10-25 13:34:32 +0200

    Refactor to use shared setup.

--- Tests/Invoke-DockerTests.Tests.ps1
+++ Tests/Invoke-DockerTests.Tests.ps1
@@ -1,14 +1,13 @@
 Import-Module -Force $PSScriptRoot/../Docker.Build.psm1
+Import-Module -Force $PSScriptRoot/../Docker.Build.Tests.psm1
 
 Describe 'Run docker tests' {
-    $testData = Join-Path (Split-Path -Parent $PSScriptRoot) "Test-Data"
-    $pesterTestData = Join-Path $testData "PesterTests"
-    $dirWithNoTest = Join-Path $testData "DockerImage"
-    $dirThatDoesNotExist = Join-Path $testData "GibberishGoo"
+    $dirWithNoTest = Join-Path $Global:TestDataDir "DockerImage"
+    $dirThatDoesNotExist = Join-Path $Global:TestDataDir "GibberishGoo"
 
     Context 'Run with 1 pester test' {
         It 'finds test files and produces correct command to run them' {
-            $result = Invoke-DockerTests -TestDirectory $pesterTestData
+            $result = Invoke-DockerTests -TestDirectory $Global:PesterTestsDir
             $result.PassedCount | Should -Be 1
             $result.TestResult[0].Describe | Should -BeExactly 'At level 0'
         }
@@ -16,7 +15,7 @@ Describe 'Run docker tests' {
 
     Context 'Run with 2 pester tests' {
         It 'finds test files and produces correct command to run them' {
-            $result = Invoke-DockerTests -TestDirectory $pesterTestData -Depth 1
+            $result = Invoke-DockerTests -TestDirectory $Global:PesterTestsDir -Depth 1
             $result.PassedCount | Should -Be 2
             $result.TestResult[0].Describe | Should -BeExactly 'At level 1'
             $result.TestResult[1].Describe | Should -BeExactly 'At level 0'
@@ -32,25 +31,16 @@ Describe 'Run docker tests' {
 
     Context 'Run with dir that does not exist' {
         It 'finds test files and produces correct command to run them' {
-            $code = {Invoke-DockerTests -TestDirectory $dirThatDoesNotExist}
+            $code = { Invoke-DockerTests -TestDirectory $dirThatDoesNotExist }
             $code | Should -Throw -ExceptionType ([System.IO.DirectoryNotFoundException]) -PassThru
         }
     }
 
     Context 'Pipeline execution' {
 
-        BeforeAll {
-            $pipedInput = {
-                $input = [PSCustomObject]@{
-                }
-                return $input
-            }
-        }
-
         It 'returns the expected pscustomobject' {
-            $result = Invoke-DockerTests -TestDirectory $pesterTestData
+            $result = Invoke-DockerTests -TestDirectory $Global:PesterTestsDir
             $result.TestResult | Should -Not -BeNullOrEmpty
         }
     }
-
 }
