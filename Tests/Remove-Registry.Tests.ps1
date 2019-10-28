7b515882e67e85a436ea59e79cc81ff3f4f7c3e5
Rasmus Wihlborg Jelsgaard <jelsgaard@gmail.com>
2019-10-28 11:51:12 +0100

    Add test for Remove-Registry.

---
+++ Tests/Remove-Registry.Tests.ps1
@@ -0,0 +1,25 @@
+Import-Module -Force $PSScriptRoot/../Docker.Build.psm1
+. "$PSScriptRoot\..\Private\Remove-Prefix.ps1"
+
+Describe 'Remove a prefix' {
+
+    Context 'When input is supplied' {
+
+        It 'can remove the prefix on valid input when prefix not found in the string' {
+            $input = "test"
+
+            $result = Remove-Prefix -Value $input -Prefix 'notfound'
+
+            $result | Should -BeExactly $input
+        }
+
+
+        It 'can remove the prefix on valid input when prefix is entire string' {
+            $input = "test"
+
+            $result = Remove-Prefix -Value $input -Prefix 'test'
+
+            $result | Should -BeExactly ''
+        }
+    }
+}
