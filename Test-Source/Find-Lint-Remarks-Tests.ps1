d77e054a6dbaef7cad194f7f24c4d9392a2aaa22
Rasmus Wihlborg Jelsgaard <jelsgaard@gmail.com>
2019-10-29 11:22:33 +0100

    Cleanup

--- Test-Source/Find-Lint-Remarks-Tests.ps1
+++ Test-Source/Find-Lint-Remarks-Tests.ps1
@@ -7,7 +7,6 @@ Describe 'Parse context from git repository' {
 
     Context 'When parsing text as lint remarks' {
 
-
         It 'can find 5 lint remarks' {
             $validText = @"
 /dev/stdin:2 DL3027 Do not use apt as it is meant to be a end-user tool, use apt-get or apt-cache instead /dev/stdin:3 DL3027 Do not use apt as it is meant to be a end-user tool, use apt-get or apt-cache instead /dev/stdin:8 DL3009 Delete the apt-get lists after installing something /dev/stdin:11 DL3027 Do not use apt as it is meant to be a end-user tool, use apt-get or apt-cache instead /dev/stdin:12 SC1025 Use arguments JSON notation for CMD and ENTRYPOINT arguments
@@ -33,7 +32,6 @@ sdf sdfsdf sdf sdfsd
 
             $result.Length | Should -Be 1
         }
-
     }
 }
 
