98ca2a665f1f69e01b50d088516921ff9ba05961
Joseph Petersen <joseph.petersen@3shape.com>
2019-10-30 22:08:55 +0100

    skip dotnet install if already installed

--- Install-Prerequisites.ps1
+++ Install-Prerequisites.ps1
@@ -4,3 +4,11 @@ Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 Install-Module -Name psake -MinimumVersion 4.9.0 -Repository "PSGallery" -Force
 Install-Module -Name pester -MinimumVersion 4.9.0 -Repository "PSGallery" -Force
 Install-Module -Name PSCodeCovIo -MinimumVersion 1.0.1 -Repository "PSGallery" -Force
+
+if ($env:TRAVIS -eq 'true') {
+    if (Test-Path -Path "$env:HOME/.dotnet/tools/dotnet-gitversion") {
+        Write-Host "GitVersion already installed"
+    } else {
+        & dotnet tool install --global GitVersion.Tool --version 5.1.1
+    }
+}
