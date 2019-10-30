2413570c8868f1a085487a699f058e1f006c677d
Rasmus Wihlborg Jelsgaard <jelsgaard@gmail.com>
2019-10-30 11:02:22 +0100

    Support pass-thru

--- Source/Public/Invoke-DockerBuild.ps1
+++ Source/Public/Invoke-DockerBuild.ps1
@@ -39,7 +39,9 @@ function Invoke-DockerBuild {
         "CommandResult" = $commandResult
     }
     if ($PassThru) {
-        Write-Host $commandResult.Output
+        foreach ($line in $($commandResult.Output)) {
+            Write-Information $line
+        }
     }
     return $result
 }
