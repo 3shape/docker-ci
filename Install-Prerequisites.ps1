8ab619d703d3a79826399ccc5b0e959ddf132315
Joseph Petersen <joseph.petersen@3shape.com>
2019-10-30 18:39:27 +0100

    Please use current user scope!

--- Install-Prerequisites.ps1
+++ Install-Prerequisites.ps1
@@ -1,5 +1,5 @@
 # The psake module is needed to run tests and publish the module to powershell gallery.
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-Install-Module -Name psake -MinimumVersion 4.9.0 -Repository "PSGallery"
-Install-Module -Name pester -MinimumVersion 4.9.0 -Repository "PSGallery"
-Install-Module -Name PSCodeCovIo -MinimumVersion 1.0.1 -Repository "PSGallery"
+Install-Module -Name psake -MinimumVersion 4.9.0 -Repository "PSGallery" -Scope CurrentUser -Force
+Install-Module -Name pester -MinimumVersion 4.9.0 -Repository "PSGallery" -Scope CurrentUser -Force
+Install-Module -Name PSCodeCovIo -MinimumVersion 1.0.1 -Repository "PSGallery" -Scope CurrentUser -Force
