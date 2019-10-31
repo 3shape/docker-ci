78eade5ca3de32bcdd418da519e6dee7358be398
Joseph Petersen <josephp90@gmail.com>
2019-10-31 07:22:23 +0100

    Revert `-force` :sweat:

--- Install-Prerequisites.ps1
+++ Install-Prerequisites.ps1
@@ -1,9 +1,9 @@
 #Requires -Version 6
 # The psake module is needed to run tests and publish the module to powershell gallery.
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-Install-Module -Name psake -MinimumVersion 4.9.0 -Repository "PSGallery" -Force
-Install-Module -Name pester -MinimumVersion 4.9.0 -Repository "PSGallery" -Force
-Install-Module -Name PSCodeCovIo -MinimumVersion 1.0.1 -Repository "PSGallery" -Force
+Install-Module -Name psake -MinimumVersion 4.9.0 -Repository "PSGallery"
+Install-Module -Name pester -MinimumVersion 4.9.0 -Repository "PSGallery"
+Install-Module -Name PSCodeCovIo -MinimumVersion 1.0.1 -Repository "PSGallery"
 
 if ($env:TRAVIS -eq 'true') {
     if (Test-Path -Path "$env:HOME/.dotnet/tools/dotnet-gitversion") {
