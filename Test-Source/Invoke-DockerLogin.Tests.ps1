564875fa0e1dfd285dca3bd459f5c96a919e66b9
Khang Yeen Lee <KhangYeen.Lee@3shape.com>
2019-11-02 14:55:09 +0100

    Password no longer leaks when login fails.

--- Test-Source/Invoke-DockerLogin.Tests.ps1
+++ Test-Source/Invoke-DockerLogin.Tests.ps1
@@ -10,61 +10,65 @@ Describe 'Login failure' {
 
         BeforeEach {
             Initialize-MockReg
-            $amok = { # Do not throw exception }
-                Mock -CommandName "Invoke-Command" $Global:CodeThatReturnsExitCodeOne -Verifiable -ModuleName $Global:ModuleName
-                Mock -CommandName "Assert-ExitCodeOK" $amok -Verifiable -ModuleName $Global:ModuleName
+            $assertExitCodeOkMocked = {
+                # Capture the masked command
+                StoreMockValue -Key "maskedCommand" -Value $Result.Command
+                throw [System.Exception]::new()
             }
+            Mock -CommandName "Invoke-Command" $Global:CodeThatReturnsExitCodeOne -Verifiable -ModuleName $Global:ModuleName
+            Mock -CommandName "Assert-ExitCodeOK" $assertExitCodeOkMocked -Verifiable -ModuleName $Global:ModuleName
+        }
 
-            it 'should mask password from the logs when login fails after thrown exception' {
-                # $loginCode = { Invoke-DockerLogin -Username "Mocked" -Password (ConvertTo-SecureString 'MockedPassword' –asplaintext –force) }
-                # $result = $loginCode | Should -BeExactly 'Write-Output "*********" | docker login --username "Mocked" --password-stdin'
-                $result = Invoke-DockerLogin -Username "Mocked" -Password (ConvertTo-SecureString 'MockedPassword' –asplaintext –force)
-                $result | Should -BeExactly 'Write-Output "*********" | docker login --username "Mocked" --password-stdin'
-            }
+        it 'should mask password from the logs when login fails after thrown exception' {
+            $loginCode = { Invoke-DockerLogin -Username "Mocked" -Password (ConvertTo-SecureString 'MockedPassword' –asplaintext –force) }
+            $loginCode | Should -Throw -ExceptionType ([System.Exception]) -PassThru
+            $result = GetMockValue -Key 'maskedCommand'
+            $result | Should -BeExactly 'Write-Output "*********" | docker login --username "Mocked" --password-stdin'
+        }
+    }
+}
+
+Describe 'Docker login ' {
+
+    BeforeEach {
+        Initialize-MockReg
+        Mock -CommandName "Invoke-Command" $Global:CodeThatReturnsExitCodeZero -Verifiable -ModuleName $Global:ModuleName
+    }
+
+    Context 'Login to default docker registry' {
+
+        It 'produced the correct command to invoke' {
+            Invoke-DockerLogin -Username "Mocked" -Password (ConvertTo-SecureString 'MockedPassword' –asplaintext –force)
+            $result = GetMockValue -Key 'command'
+            $result | Should -BeExactly 'Write-Output "MockedPassword" | docker login --username "Mocked" --password-stdin'
         }
     }
 
-    Describe 'Docker login ' {
+    Context 'Login to specific docker registry' {
 
-        BeforeEach {
-            Initialize-MockReg
+        It 'produced the correct command to invoke' {
+            Invoke-DockerLogin -Registry 'my.docker.registry' -Username "Mocked" -Password (ConvertTo-SecureString 'MockedPassword' –asplaintext –force)
+            $result = GetMockValue -Key 'command'
+            $result | Should -BeExactly 'Write-Output "MockedPassword" | docker login --username "Mocked" --password-stdin my.docker.registry'
+        }
+
+        It 'produced the correct command to invoke, with $null registry parameter' {
+            Invoke-DockerLogin -Registry $null -Username "Mocked" -Password (ConvertTo-SecureString 'MockedPassword' –asplaintext –force)
+            $result = GetMockValue -Key 'command'
+            $result | Should -BeExactly 'Write-Output "MockedPassword" | docker login --username "Mocked" --password-stdin'
+        }
+    }
+
+    Context 'Passthru execution' {
+
+        it 'can redirect output' {
+            $tempFile = New-TemporaryFile
             Mock -CommandName "Invoke-Command" $Global:CodeThatReturnsExitCodeZero -Verifiable -ModuleName $Global:ModuleName
-        }
 
-        Context 'Login to default docker registry' {
+            Invoke-DockerLogin -Username "Mocked" -Password (ConvertTo-SecureString 'MockedPassword' –asplaintext –force) -Passthru 6> $tempFile
+            $result = Get-Content $tempFile
 
-            It 'produced the correct command to invoke' {
-                Invoke-DockerLogin -Username "Mocked" -Password (ConvertTo-SecureString 'MockedPassword' –asplaintext –force)
-                $result = GetMockValue -Key 'command'
-                $result | Should -BeExactly 'Write-Output "MockedPassword" | docker login --username "Mocked" --password-stdin'
-            }
-        }
-
-        Context 'Login to specific docker registry' {
-
-            It 'produced the correct command to invoke' {
-                Invoke-DockerLogin -Registry 'my.docker.registry' -Username "Mocked" -Password (ConvertTo-SecureString 'MockedPassword' –asplaintext –force)
-                $result = GetMockValue -Key 'command'
-                $result | Should -BeExactly 'Write-Output "MockedPassword" | docker login --username "Mocked" --password-stdin my.docker.registry'
-            }
-
-            It 'produced the correct command to invoke, with $null registry parameter' {
-                Invoke-DockerLogin -Registry $null -Username "Mocked" -Password (ConvertTo-SecureString 'MockedPassword' –asplaintext –force)
-                $result = GetMockValue -Key 'command'
-                $result | Should -BeExactly 'Write-Output "MockedPassword" | docker login --username "Mocked" --password-stdin'
-            }
-        }
-
-        Context 'Passthru execution' {
-
-            it 'can redirect output' {
-                $tempFile = New-TemporaryFile
-                Mock -CommandName "Invoke-Command" $Global:CodeThatReturnsExitCodeZero -Verifiable -ModuleName $Global:ModuleName
-
-                Invoke-DockerLogin -Username "Mocked" -Password (ConvertTo-SecureString 'MockedPassword' –asplaintext –force) -Passthru 6> $tempFile
-                $result = Get-Content $tempFile
-
-                $result | Should -Be @('Hello', 'World')
-            }
+            $result | Should -Be @('Hello', 'World')
         }
     }
+}
