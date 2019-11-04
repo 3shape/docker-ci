41902b0638d1d1642721c17f743a4685189564d2
Khang Yeen Lee <KhangYeen.Lee@3shape.com>
2019-11-04 21:26:30 +0100

    Cleaned up some codes. -BeLikeExactly → -BeExactly.

--- Test-Source/Invoke-DockerBuild.Tests.ps1
+++ Test-Source/Invoke-DockerBuild.Tests.ps1
@@ -21,24 +21,22 @@ Describe 'Build docker images' {
         It 'creates correct docker build command' {
             Invoke-DockerBuild -ImageName "leeandrasmus" -Context $Global:DockerImagesDir -Dockerfile $dockerFile
             $result = GetMockValue -Key "command"
-            $result | Should -BeLikeExactly "docker build `"$($Global:DockerImagesDir)`" -t leeandrasmus:latest -f `"${dockerFile}`""
+            $result | Should -BeExactly "docker build `"$Global:DockerImagesDir`" -t leeandrasmus:latest -f `"${dockerFile}`""
         }
 
         It 'Throws exception if exitcode is not 0' {
             Mock -CommandName "Invoke-Command" $Global:CodeThatReturnsExitCodeOne -Verifiable -ModuleName $Global:ModuleName
-            $runner = {
-                Invoke-DockerBuild -ImageName "leeandrasmus" -Context $Global:DockerImagesDir -Dockerfile $dockerFile
-            }
+            $runner = { Invoke-DockerBuild -ImageName "leeandrasmus" -Context $Global:DockerImagesDir -Dockerfile $dockerFile }
             $runner | Should -Throw -ExceptionType ([System.Exception]) -PassThru
         }
     }
 
-    Context 'Docker build with various parameters' {
+    Context 'Docker build with various registry parameters' {
 
         It 'creates correct docker build command, with valid registry parameter' {
             Invoke-DockerBuild -ImageName "leeandrasmus" -Context $Global:DockerImagesDir -Dockerfile $dockerFile -Registry 'valid'
             $result = GetMockValue -Key "command"
-            $result | Should -BeLikeExactly "docker build `"$($Global:DockerImagesDir)`" -t valid/leeandrasmus:latest -f `"${dockerFile}`""
+            $result | Should -BeExactly "docker build `"$Global:DockerImagesDir`" -t valid/leeandrasmus:latest -f `"${dockerFile}`""
         }
 
         It 'creates correct docker build command, with $null registry parameter' {
