7689e8079ba3a6934be4c7c1630a9e71077ecfa8
Tuk <KhangYeen.Lee@3shape.com>
2019-11-13 12:50:17 +0100

    Feature/extra build param (#60)
    * Removed final bits of script blocks that were moved to the test module.
    * Cleaned up unused imports.
    * $dockerFile variable is mocked, not really important to be platform specific.
    * Cleaned up some codes. -BeLikeExactly → -BeExactly.
    * Added support for ExtraParams for #59
    * ExtraParams validation rule added.
    * Updated based on review comments.
    * Updated as suggested.

--- Test-Source/Invoke-DockerBuild.Tests.ps1
+++ Test-Source/Invoke-DockerBuild.Tests.ps1
@@ -3,19 +3,12 @@ Import-Module -Global -Force $PSScriptRoot/Docker.Build.Tests.psm1
 Import-Module -Global -Force $PSScriptRoot/MockReg.psm1
 
 . "$PSScriptRoot\..\Source\Private\Invoke-Command.ps1"
-. "$PSScriptRoot\..\Source\Private\CommandResult.ps1"
-. "$PSScriptRoot\..\Source\Private\Assert-ExitCodeOk.ps1"
 
 Describe 'Build docker images' {
 
     BeforeEach {
         Initialize-MockReg
-        if ($IsWindows) {
-            $dockerFile = Join-Path $Global:DockerImagesDir "Windows.Dockerfile"
-        }
-        elseif ($IsLinux) {
-            $dockerFile = Join-Path $Global:DockerImagesDir "Linux.Dockerfile"
-        }
+        $dockerFile = Join-Path $Global:DockerImagesDir "Linux.Dockerfile"
         Mock -CommandName "Invoke-Command" $Global:CodeThatReturnsExitCodeZero -Verifiable -ModuleName $Global:ModuleName
     }
 
@@ -28,30 +21,54 @@ Describe 'Build docker images' {
         It 'creates correct docker build command' {
             Invoke-DockerBuild -ImageName "leeandrasmus" -Context $Global:DockerImagesDir -Dockerfile $dockerFile
             $result = GetMockValue -Key "command"
-            $result | Should -BeLikeExactly "docker build `"$($Global:DockerImagesDir)`" -t leeandrasmus:latest -f `"${dockerFile}`""
+            $result | Should -BeExactly "docker build `"$Global:DockerImagesDir`" -t leeandrasmus:latest -f `"${dockerFile}`""
         }
 
         It 'Throws exception if exitcode is not 0' {
             Mock -CommandName "Invoke-Command" $Global:CodeThatReturnsExitCodeOne -Verifiable -ModuleName $Global:ModuleName
-            $runner = {
-                Invoke-DockerBuild -ImageName "leeandrasmus" -Context $Global:DockerImagesDir -Dockerfile $dockerFile
-            }
+            $runner = { Invoke-DockerBuild -ImageName "leeandrasmus" -Context $Global:DockerImagesDir -Dockerfile $dockerFile }
             $runner | Should -Throw -ExceptionType ([System.Exception]) -PassThru
         }
     }
 
-    Context 'Docker build with various parameters' {
+    Context 'Docker build with various registry parameters' {
 
         It 'creates correct docker build command, with valid registry parameter' {
             Invoke-DockerBuild -ImageName "leeandrasmus" -Context $Global:DockerImagesDir -Dockerfile $dockerFile -Registry 'valid'
             $result = GetMockValue -Key "command"
-            $result | Should -BeLikeExactly "docker build `"$($Global:DockerImagesDir)`" -t valid/leeandrasmus:latest -f `"${dockerFile}`""
+            $result | Should -BeExactly "docker build `"$Global:DockerImagesDir`" -t valid/leeandrasmus:latest -f `"${dockerFile}`""
         }
 
         It 'creates correct docker build command, with $null registry parameter' {
             Invoke-DockerBuild -ImageName "leeandrasmus" -Context $Global:DockerImagesDir -Dockerfile $dockerFile -Registry $null
             $result = GetMockValue -Key "command"
-            $result | Should -BeLikeExactly "docker build `"$($Global:DockerImagesDir)`" -t leeandrasmus:latest -f `"${dockerFile}`""
+            $result | Should -BeLikeExactly "docker build `"$Global:DockerImagesDir`" -t leeandrasmus:latest -f `"${dockerFile}`""
+        }
+    }
+
+    Context 'Docker build with the extra parameter' {
+
+        It 'creates correct docker build command, with the extra parameter' {
+            Invoke-DockerBuild -ImageName "leeandrasmus" -Context $Global:DockerImagesDir -Dockerfile $dockerFile -ExtraParams '--cache-from garbage-in:garbage-out'
+            $result = GetMockValue -Key "command"
+            $result | Should -BeExactly "docker build `"$Global:DockerImagesDir`" -t leeandrasmus:latest -f `"${dockerFile}`" --cache-from garbage-in:garbage-out"
+        }
+
+        It 'creates correct docker build command, with the extra parameter and  $null registry parameter' {
+            Invoke-DockerBuild -ImageName "leeandrasmus" -Context $Global:DockerImagesDir -Dockerfile $dockerFile -Registry $null -ExtraParams '-m=4g'
+            $result = GetMockValue -Key "command"
+            $result | Should -BeLikeExactly "docker build `"$Global:DockerImagesDir`" -t leeandrasmus:latest -f `"${dockerFile}`" -m=4g"
+        }
+
+        It 'creates correct docker build command, with a longer extra parameter' {
+            Invoke-DockerBuild -ImageName "leeandrasmus" -Context $Global:DockerImagesDir -Dockerfile $dockerFile -ExtraParams '--cache-from garbage-in:garbage-out -m=4g'
+            $result = GetMockValue -Key "command"
+            $result | Should -BeExactly "docker build `"$Global:DockerImagesDir`" -t leeandrasmus:latest -f `"${dockerFile}`" --cache-from garbage-in:garbage-out -m=4g"
+        }
+
+        It 'throws exception if empty extra parameter is parsed' {
+            $theCode = { Invoke-DockerBuild -ImageName "leeandrasmus" -Context $Global:DockerImagesDir -Dockerfile $dockerFile -ExtraParams '' }
+            $theCode | Should -Throw -ExceptionType ([System.Management.Automation.ParameterBindingException]) -PassThru
         }
     }
 
