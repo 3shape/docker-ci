8c0bd01a31bf87594e10a2bad6bab11d1937bc41
Rasmus Wihlborg Jelsgaard <jelsgaard@gmail.com>
2019-11-25 14:27:14 +0100

    Halt if token, endpoint or module name not set.

--- Notify-Slack.ps1
+++ Notify-Slack.ps1
@@ -1,5 +1,16 @@
 $token = $Env:SlackToken
 $module = $Env:Docker_CI_ModuleName
+
+if (!$token) {
+    throw ("token not set, cannot publish to slack.")
+}
+if (!$Env:SlackURI) {
+    thow ("Slack integration endpoint not set, cannot publish to slack.")
+}
+if (!$module) {
+    throw ("module name not set, cannot publish to slack.")
+}
+
 $slackURI = "${Env:SlackURI}=${token}"
 $version = "${Env:GitVersion_Version}${Env:GitVersion_PreReleaseTagWithDash}"
 $slackChannel = 'batcave'
