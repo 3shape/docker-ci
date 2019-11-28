c3a7f0515782c0e4e1811f95899f816ed546c395
Rasmus Wihlborg Jelsgaard <jelsgaard@gmail.com>
2019-11-28 17:25:42 +0100

    Halfway there.

--- Source/Private/Invoke-Command.ps1
+++ Source/Private/Invoke-Command.ps1
@@ -1,22 +1,80 @@
+# Copied from here: https://github.com/dotnet/roslyn/blob/master/src/Setup/Installer/tools/utils.ps1
+
 function Invoke-Command {
-    [CmdletBinding()]
+    [CmdletBinding(PositionalBinding = $false)]
     param (
-        [Parameter(mandatory = $true)]
+        [Parameter(mandatory = $true, Position = 0)]
+        [String] $Command,
+
+        [Parameter(Position = 1)]
         [String]
-        $Command
+        $CommandArgs = '',
+
+        [Switch]
+        $Quiet = [System.Convert]::ToBoolean($env:DOCKER_CI_QUIET_MODE)
     )
-    $backupErrorActionPreference = $script:ErrorActionPreference
-    $script:ErrorActionPreference = "Continue"
+
+    $result = [CommandCoreResult]::new()
+    $result.Command = $Command
+    $result.CommandArgs = $CommandArgs
+    $result.ExitCode = -1
+
+    [System.Diagnostics.Process] $process = New-Process -Command $Command -Arguments $CommandArgs -WorkingDirectory (Get-Location)
+
     try {
-        $result = [CommandResult]::new()
-        $result.Command = $Command
-        Write-Debug "Executing command: ${Command}"
-        $outputs = Invoke-Expression "& $Command 2>&1"
-        $result.Success = $?
-        $result.ExitCode = $lastexitcode
-        $result.Output = $outputs
+        $oStdOutBuilder = [System.Text.StringBuilder]::new()
+        $oStdErrBuilder = [System.Text.StringBuilder]::new()
+
+        $finished = $false
+
+        $eventHandlerSource = 'if (! [String]::IsNullOrEmpty($EventArgs.Data)) {
+                $Event.MessageData.AppendLine($EventArgs.Data)
+                if (WriteLogging) {
+                    Write-Information -InformationAction "Continue" -MessageData $EventArgs.Data
+                }
+            }'
+        $eventHandlerSource = $eventHandlerSource.Replace("WriteLogging", !$Quiet)
+
+        [scriptblock] $eventHandler = [scriptblock]::Create($eventHandlerSource)
+        $sScripBlock = {
+            if (! [String]::IsNullOrEmpty($EventArgs.Data)) {
+                $Event.MessageData.AppendLine($EventArgs.Data)
+                Write-Information -InformationAction 'Continue' -MessageData $EventArgs.Data
+            }
+        }
+
+
+        $stdOutEventHandler = Register-ObjectEvent -InputObject $process `
+            -Action $eventHandler -EventName 'OutputDataReceived' `
+            -MessageData $oStdOutBuilder
+
+        $stdErrEventHandler = Register-ObjectEvent -InputObject $process `
+            -Action $eventHandler -EventName 'ErrorDataReceived' `
+            -MessageData $oStdErrBuilder
+
+        $process.Start() | Out-Null
+        $process.BeginOutputReadLine()
+        $process.BeginErrorReadLine()
+
+
+        while (-not $process.WaitForExit(100)) {
+            # Allow interrupts like CTRL + C
+        }
+        $finished = $true
+    } catch {
+        Write-Debug 'bad'
     } finally {
-        $script:ErrorActionPreference = $backupErrorActionPreference
+        Unregister-Event -SourceIdentifier $stdOutEventHandler.Name
+        Unregister-Event -SourceIdentifier $stdErrEventHandler.Name
+        # If we didn't finish then an error occurred or the user hit ctrl-c.  Either
+        # way kill the process
+        if (-not $finished -and -not $process.HasExited) {
+            Write-Debug "Cleanup, kill the process with id $($process.Id)"
+            $process.Kill()
+        }
     }
+    $result.Output = $oStdOutBuilder.ToString().Trim()
+    $errors = $oStdErrBuilder.ToString().Trim()
+    $result.ExitCode = $process.ExitCode
     return $result
 }
