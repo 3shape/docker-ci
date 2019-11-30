b8e3bad44b8cdde984f18d569f07ee6fc327f9c6
Rasmus Wihlborg Jelsgaard <jelsgaard@gmail.com>
2019-11-30 10:54:01 +0100

    Update test

--- Test-Source/Find-ImageName.Tests.ps1
+++ Test-Source/Find-ImageName.Tests.ps1
@@ -45,7 +45,7 @@ Describe 'Parse context from git repository' {
             $gitReposWithUppercase = Join-Path $tempFolder 'ThisIsNotUsefulAsAnImageName'
             New-FakeGitRepository -Path $gitReposWithUppercase
             $mockCode = {
-                $invocationResult = [CommandResult]::new();
+                $invocationResult = [CommandCoreResult]::new();
                 $invocationResult.Output = "https://github.com/3shapeAS/DOCKERBUILD-pwsh.git"
                 $invocationResult
             }
