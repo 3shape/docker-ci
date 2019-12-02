29c6b94b9584823d999208241037d28ba0f6c5ec
Rasmus Wihlborg Jelsgaard <jelsgaard@gmail.com>
2019-12-02 14:36:14 +0100

    Remove unused code.

--- Test-Source/Invoke-Command.Tests.ps1
+++ Test-Source/Invoke-Command.Tests.ps1
@@ -1,9 +1,5 @@
 Import-Module -Force $PSScriptRoot/../Source/Docker-CI.psm1
-Import-Module -Force $PSScriptRoot/Docker-CI.Tests.psm1
-
-. "$PSScriptRoot\..\Source\Private\Invoke-Command.ps1"
-. "$PSScriptRoot\..\Source\Private\CommandResult.ps1"
-. "$PSScriptRoot\..\Source\Private\New-Process.ps1"
+Import-Module -Force -Global $PSScriptRoot/Docker-CI.Tests.psm1
 
 Describe 'Runs only external tools' {
 
