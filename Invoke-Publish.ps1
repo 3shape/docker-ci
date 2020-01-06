797d6a534ad3bd88326fb4031b698038eb0eff9e
Joseph Petersen <josephp90@gmail.com>
2020-01-06 23:39:20 +0100

    Publish does not need to depend on build

--- Invoke-Publish.ps1
+++ Invoke-Publish.ps1
@@ -1,3 +1,3 @@
 Get-Module Docker.Build | Remove-Module
-Invoke-Psake -buildFile "$PSScriptRoot/Build.ps1" -taskList build, test, publish
+Invoke-Psake -buildFile "$PSScriptRoot/Build.ps1" -taskList build, publish
 exit ( [int]( -not $psake.build_success ) )
