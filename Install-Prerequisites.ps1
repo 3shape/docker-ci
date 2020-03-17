ef6c11e344c12189d505b7324deab1ee3dfaae07
Rasmus Wihlborg Jelsgaard <jelsgaard@gmail.com>
2020-03-17 20:01:18 +0100

    Fix wrong order of args
    Mistakenly added -Force in the wrong place.

--- Install-Prerequisites.ps1
+++ Install-Prerequisites.ps1
@@ -1,6 +1,6 @@
 #Requires -Version 6
 # The psake module is needed to run tests and publish the module to powershell gallery.
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-Install-Module -Name -Force psake -MinimumVersion 4.9.0 -Repository "PSGallery"
-Install-Module -Name -Force pester -MinimumVersion 4.9.0 -Repository "PSGallery"
-Install-Module -Name -Force PSCodeCovIo -MinimumVersion 1.0.1 -Repository "PSGallery"
+Install-Module -Force -Name psake -MinimumVersion 4.9.0 -Repository "PSGallery"
+Install-Module -Force -Name pester -MinimumVersion 4.9.0 -Repository "PSGallery"
+Install-Module -Force -Name PSCodeCovIo -MinimumVersion 1.0.1 -Repository "PSGallery"
